e7a9dc9908a59209751616e030c35fa7175b06bf
Tanvi Penumudy <46785609+tanvipenumudy@users.noreply.github.com>
2021-01-27 15:52:50 +0530

    Pushed Files to Repo

---
+++ Modified for SVM-Naive Bayes/app.py
@@ -0,0 +1,81 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import joblib
+import numpy as np
+
+model = joblib.load('NB.pkl')
+import json
+import random
+intents = json.loads(open('data.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+    
+    
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if(w==s):
+                bag[i] = 1
+                if(show_details):
+                    print("found in bag: %s" % w)
+    return(np.array(bag))
+    
+
+def predict_class(sentence, model):
+    
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    print(res)
+    res1 = [0 for i in range(9)]
+    res1[res] = 1
+    res = res1
+    print(res)
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+    
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']==tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+    
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+    
+if __name__ == "__main__":
+    app.run()
+    
